9d1a9614dec82cd9a952d364a594b67a686ff50e
BenjaminMenendez7 <benjamenendez7@gmail.com>
2025-12-04 15:24:18 -0300

    Fix final

--- microservicios/camiones-service/src/main/java/utnfc/isi/back/camionesservice/repository/TipoCamionRepository.java
+++
@@ -1,7 +0,0 @@
-package utnfc.isi.back.camionesservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import utnfc.isi.back.camionesservice.entity.TipoCamion;
-
-public interface TipoCamionRepository extends JpaRepository<TipoCamion, Long> {
-}
