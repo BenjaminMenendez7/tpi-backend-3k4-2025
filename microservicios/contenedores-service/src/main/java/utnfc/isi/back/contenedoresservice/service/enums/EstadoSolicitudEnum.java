ed4aa7a50cd9d0bd0ef0157a689d31c9ff517e17
BenjaminMenendez7 <benjamenendez7@gmail.com>
2025-12-04 03:02:19 -0300

    Fix actualización EstadoSolicitudEnum

--- microservicios/contenedores-service/src/main/java/utnfc/isi/back/contenedoresservice/service/enums/EstadoSolicitudEnum.java
+++ microservicios/contenedores-service/src/main/java/utnfc/isi/back/contenedoresservice/service/enums/EstadoSolicitudEnum.java
@@ -1,15 +1,16 @@
 package utnfc.isi.back.contenedoresservice.service.enums;
 
+import lombok.Getter;
+
 import java.util.Arrays;
 
+@Getter
 public enum EstadoSolicitudEnum {
     BORRADOR(1L), PROGRAMADA(2L), EN_TRANSITO(3L), ENTREGADA(4L);
 
     private final Long id;
 
     EstadoSolicitudEnum(Long id) { this.id = id; }
-
-    public Long getId() { return id; }
 
     public static EstadoSolicitudEnum fromId(Long id) {
         return Arrays.stream(values())
